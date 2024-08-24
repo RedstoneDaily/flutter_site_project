7bb4925885463d55e36260b21b4f24c9389f1b19
IAALAI <iaalai@iaalai.cn>
2024-08-24 11:34:56 +0000

    fix build

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 function get_flutter() {
-    sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install -y curl git unzip xz-utils zip libglu1-mesa
-    wget https://storage.flutter-io.cn/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.1-stable.tar.xz
-    tar -xf flutter_linux_3.24.1-stable.tar.xz -C /usr/bin/
+    wget https://storage.flutter-io.cn/flutter_infra_release/releases/stable/linux/flutter_linux_3.24.1-stable.tar.xz -O flutter.tar.xz
+    tar -xf flutter.tar.xz -C ./
+    rm flutter.tar.xz
+    export PATH=$(pwd)/flutter/bin:$PATH
 }
 
 function check_flutter() {
@@ -10,7 +11,8 @@ function check_flutter() {
         echo "flutter not have,auto install"
         get_flutter
     else
-        # echo "flutter found"
+        echo "flutter found"
+    fi
 }
 
 check_flutter
